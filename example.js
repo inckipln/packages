0024d9e374b0a272952a701e4f9b201661a22bdc
Nayram Mensah <nayrammensah@gmail.com>
2019-03-07 02:58:45 +0000

    update pub_key

--- example.js
+++ example.js
@@ -2,7 +2,7 @@
 
 const payment = require ('./asoriba')
 
-const AUTH_TOKEN = "K_iTUWzcQqUXGH9ZbDxIg1NhLZCZ0VrUdw___FAPK5jXkG1B1SaN-qrnwKvI"
+const AUTH_TOKEN = "hkhgkhjhgjkhg"
 
 const BASE_URL = "https://paymentsandbox.asoriba.com"
  
@@ -15,7 +15,7 @@ const options = {
     },
     callback:"https://435c02e7.ngrok.io/kipln/callback",
     post_url:"https://435c02e7.ngrok.io/kipln",
-    pub_key:"K_iTUWzcQqUXGH9ZbDxIg1NhLZCZ0VrUdw___FAPK5jXkG1B1SaN-qrnwKvI",
+    pub_key:"khgjhgkghvhjgfgh",
     order_image_url:"https://payment.asoriba.com/assets/asoribalogo-3d4540003815aee230ca676138579ed495cfa975270fe2d7e656292c4508d472.png",
     first_name:"Nayram",
     last_name:"Mensah",
